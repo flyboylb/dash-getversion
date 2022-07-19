cbd64dd0b6265e534d681803f5bcb368f60ea1f8
Patrick Delifer <pdelifer@gmail.com>
2022-07-19 09:27:23 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -13433,15 +13433,15 @@ run();
 
 function generateNextReleaseTag() {
     try {
-        const github_token = core.getInput("github_token");
-        const octokit = github.getOctokit(github_token);
-        const { owner, repo } = github.context.repo;
+        const github_token = getInput("github_token");
+        const octokit = conext.getOctokit(github_token);
+        const { owner, repo } = conext.context.repo;
         const response = octokit.repos.getLatestRelease({
             owner,
             repo,
         });
         const { tag_name: oldReleaseTag } = response.data;
-        const newReleaseTag = getNewReleaseTag(oldReleaseTag);
+      //  const newReleaseTag = getNewReleaseTag(oldReleaseTag);
         console.log(`Previous Release Tag: ${oldReleaseTag}`);
         console.log(`New Release Tag: ${newReleaseTag}`);
         
